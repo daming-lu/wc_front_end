9beac9cc74def1c497fe71e4698b311f493d56af
damingl <damingl@zoosk.com>
2014-06-01 23:35:23 -0700

    added login and signup

--- js/login.js
+++ js/login.js
@@ -1,3 +1,16 @@
 /**
  * Created by damingl on 6/1/14.
  */
+
+var loginApp = angular.module('loginApp', []);
+
+loginApp.controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.isLogin = true;
+
+    $scope.switchToRegister = function () {
+        $scope.isLogin = false;
+    };
+    $scope.switchToLogin = function () {
+        $scope.isLogin = true;
+    };
+}]);
